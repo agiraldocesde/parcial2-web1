a33477d05221052f500c79015b18e95b115957f0
andresg12334 <giraldoblandonandresfelipe@gmail.com>
2026-04-23 19:25:48 -0500

    Se agrega funcion obtenerPrioridad y ordenarAlertasPorPrioridad

--- controllers/motorLogico.js
+++ controllers/motorLogico.js
@@ -31,3 +31,35 @@ function mostrarTabla(mostrar, titulo) {
     }
     console.log('');
 }
+
+
+
+function obtenerPrioridad(severidad) {
+    if (severidad === 'roja') return 1;
+    if (severidad === 'naranja') return 2;
+    if (severidad === 'amarilla') return 3;
+    return 99;
+}
+
+function ordenarAlertasPorPrioridad(arrAlertas) {
+    let copia = [];
+    for (let i = 0; i < arrAlertas.length; i++) {
+        copia[i] = arrAlertas[i];
+    }
+
+    let n = copia.length;
+    for (let i = 0; i < n - 1; i++) {
+        for (let j = 0; j < n - i - 1; j++) {
+            let prioridadA = obtenerPrioridad(copia[j].severidad);
+            let prioridadB = obtenerPrioridad(copia[j + 1].severidad);
+            
+            if (prioridadA > prioridadB) {
+                let temp = copia[j];
+                copia[j] = copia[j + 1];
+                copia[j + 1] = temp;
+            }
+        }
+    }
+    
+    return copia;
+}
